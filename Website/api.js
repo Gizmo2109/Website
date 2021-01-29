0e5a01f7566553eb29c120ad9ff6b1961759df74
Gizmo2109 <oskar.herrmann@arcor.de>
2021-01-29 10:15:20 +0100

    first commit

---
+++ Website/api.js
@@ -0,0 +1,28 @@
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function fetchAPI() {
+    const urlAPI = 'http://localhost:8010/proxy/' + getRandom(1, 100)
+    try {
+        const response = await fetch(urlAPI)
+        const json = await response.json();
+        var obj = json.image_files
+        let test = obj.reduce((max, game) => max.height > game.height ? max : game);
+        var test1 = 'http:' + test.file_url
+        console.log(test1)
+        if (test1.charAt(test1.length-1) === 'f') {
+            await fetchAPI()
+        } else {return test1}
+    } catch(err) {
+        console.error(err);
+    }
+    
+}
+
+async function changeIMG(id) {
+    document.getElementById(id).src = await fetchAPI()
+}
+
